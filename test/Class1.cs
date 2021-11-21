8ef9250c6c793f9110b3908c58d652675df44b32
YaroslavBuchnev <92785358+YaroslavBuchnev@users.noreply.github.com>
2021-11-21 16:05:12 +0300

    Create Class1.cs

---
+++ test/Class1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibraryPassword
+{
+    public class PasswordChecker
+    {
+        public static bool validatePassword(string password)
+        {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+            if (!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            if (!password.Intersect("#$%^&_".Count() == 0)
+                return false;
+
+                return true;
+        }
+
+        static void Main(string[] args)
+        {
+            bool arr = PasswordChecker.validatePassword("1лфвлафлвафвал");
+            Console.WriteLine("res = " + arr);
+
+            Console.ReadKey();
+        }
+
+    }
+}
